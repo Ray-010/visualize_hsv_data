b5afb013de1bbb37be90327fd6acdd2d8af71f5d
unknown <ray010@outlook.jp>
2022-08-03 12:19:45 +0900

    collect data

--- get_hsv.py
+++ get_hsv.py
@@ -1,27 +1,19 @@
 import cv2
 import csv
 import numpy as np
+from csv import writer
 
 
 img = cv2.imread("./images/a_pedestrian_light_image4.jpg")
 img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 save_hsv_path = "./data/hsv_data.csv"
-csv_file = np.loadtxt(save_hsv_path)
-
 def main():
-    # img_width = img.shape[1]
-    # # for i in range(img_width):
-    data = img_hsv[0][0:5]
-
-    print(data[0][0])
-    data = np.vstack((csv_file, data))
-    print(data[0][0])
-
-    np.savetxt(save_hsv_path, data, fmt="%d")
-
+    img_height = img_hsv.shape[0]
+    for i in range(img_height):
+        data = img_hsv[i]
+        with open(save_hsv_path, "a", newline="") as file:
+            writer_obj = writer(file)
+            writer_obj.writerows(data)
 
 if __name__ == "__main__":
     main()
-
-
-

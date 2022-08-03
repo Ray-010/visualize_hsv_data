f7ae488e83ab4cb73c1e3e339414cf2d7623590b
unknown <ray010@outlook.jp>
2022-08-03 17:28:16 +0900

    visualize hue sat val

--- collect_data.py
+++ collect_data.py
@@ -11,18 +11,36 @@ S: 0-255
 V: 0-255
 """
 hue = [0]*180
-saturation = [0]*255
-value = [0]*255
+saturation = [0]*256
+value = [0]*256
 csv_data_path = "./data/hsv_data.csv"
-
 csv_file = open(csv_data_path)
 
 for row in csv.reader(csv_file):
     hue[int(row[0])] += 1
+    saturation[int(row[1])] += 1
+    value[int(row[2])] += 1
 
-x_pos = np.arange(180)
+row = 1 # グラフの行数
+col = 3 # グラフの列数
+
+hue_x = np.arange(180)
+sat_x = np.arange(256)
+val_x = np.arange(256)
 
 print(hue)
+print(saturation)
+print(value)
+fig = plt.figure()
 
-plt.bar(x_pos, hue)
+ax1 = fig.add_subplot(1, 3 ,1)
+ax2 = fig.add_subplot(1, 3 ,2)
+ax3 = fig.add_subplot(1, 3 ,3)
+
+ax1.bar(hue_x, hue)
+ax2.bar(sat_x, saturation)
+ax3.bar(val_x, value)
+
+fig.tight_layout()
+fig.savefig("results/result.jpg")
 plt.show()
